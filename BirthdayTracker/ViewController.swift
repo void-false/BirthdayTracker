dbb92271920b778b219ec1b18e4a619530053e54
Arteom Baranowski <void.false@gmail.com>
2018-05-31 01:37:13 +0300

    First commit

--- BirthdayTracker/ViewController.swift
+++ BirthdayTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Here there be Dragons
     }
 
     override func didReceiveMemoryWarning() {
